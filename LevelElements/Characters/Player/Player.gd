ec2b2dfd4c33651b7878d21b8a21b5eb5627b759
Nanthno <midnightquack@gmail.com>
2021-01-08 19:00:26 -0600

    add movement priming

--- LevelElements/Characters/Player/Player.gd
+++ LevelElements/Characters/Player/Player.gd
@@ -4,6 +4,9 @@ signal moved ()
 signal set_warming(is_warming)
 
 var is_moving = false
+var move_primed:Vector2
+var move_prime_expire = 0.2
+var move_prime_time = move_prime_expire+1
 var can_move = true
 var pushing = []
 var grid_position:Vector2
@@ -23,10 +26,10 @@ func connect_signals():
 	Global.get_level_root().connect("out_of_warmth", self, "on_out_of_warmth")
 	Global.get_level_root().connect("level_finished", self, "on_level_finished")
 
+
 func _physics_process(_delta):
 	if is_moving:
-		var move_delta = lerp(position, grid_position*grid_size, 0.001) - position
-		
+		var move_delta = lerp(position, grid_position*grid_size, 0.003) - position
 		for i in range(len(pushing)):
 			var cube = pushing[i]
 			if position.distance_to(grid_position * grid_size) < 1:
@@ -36,27 +39,36 @@ func _physics_process(_delta):
 				cube.get_node("SideDetect").position = Vector2(40,35)
 			else:
 				var side_pos = cube.get_node("SideDetect").global_position
-				cube.position = position + (move_delta.normalized() * grid_size)*(i+1)
+				cube.position = position + (move_delta * grid_size)*(i+1)
 				cube.get_node("SideDetect").global_position = side_pos
-			
-		
 
-	
-func _process(_delta):
-	if can_move:
-		get_input()
-	
+
+func _process(delta):
+	get_input()
+	move_prime_time += delta
+	if not is_moving and move_prime_time < move_prime_expire and can_move:
+		try_move(move_primed)
+
+
 func get_input():
-	if Input.is_action_just_pressed("move_left") and not is_moving:
-		try_move(Vector2.LEFT)
-	if Input.is_action_just_pressed("move_right") and not is_moving:
-		try_move(Vector2.RIGHT)
-	if Input.is_action_just_pressed("move_up") and not is_moving:
-		try_move(Vector2.UP)
-	if Input.is_action_just_pressed("move_down") and not is_moving:
-		try_move(Vector2.DOWN)
+	var dir = Vector2.ZERO
+	if Input.is_action_just_pressed("move_left"):
+		dir = Vector2.LEFT
+	if Input.is_action_just_pressed("move_right"):
+		dir = Vector2.RIGHT
+	if Input.is_action_just_pressed("move_up"):
+		dir = Vector2.UP
+	if Input.is_action_just_pressed("move_down"):
+		dir = Vector2.DOWN
+		
+	if dir != Vector2.ZERO:
+		move_primed = dir
+		move_prime_time = 0
 
 func try_move(direction:Vector2):
+	move_prime_time = move_prime_expire +1
+	move_primed = Vector2.ZERO
+	
 	pushing = $SideDetect.can_move(direction)
 	if len(pushing) > 0:
 		is_moving = true
